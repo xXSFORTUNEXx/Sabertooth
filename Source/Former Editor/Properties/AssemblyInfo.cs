ac9d16e696dde20353ac5920ccc3cd9da333fedf
Steven M. Fortune <sfortune@live.com>
2016-11-28 21:18:55 -0600

    Renamed Editor to Former Editor due to it no longer being the engines primary editor anymore. Created a new project with .NET framework instead of GWEN (faster development).

--- Source/Former Editor/Properties/AssemblyInfo.cs
+++ Source/Former Editor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Editor")]
+[assembly: AssemblyTitle("Map Editor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Editor")]
+[assembly: AssemblyProduct("Map Editor")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("570dac81-ef23-4070-8e96-0b44c2309966")]
+[assembly: Guid("e322b58d-372d-447a-b8d0-87bd3567e3b6")]
 
 // Version information for an assembly consists of the following four values:
 //
